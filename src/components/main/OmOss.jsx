0f84e895f198a0b5f945700d76d68f16f6b1ffb7
DennisNevback <74974974+DennisNevback@users.noreply.github.com>
2023-01-27 12:39:07 +0100

    Images
    added images

--- src/components/main/OmOss.jsx
+++ src/components/main/OmOss.jsx
@@ -3,5 +3,14 @@ export default function OmOss() {
   return <>
     <h1>Filmvisarna</h1>
     <p>Filmvisarna är en nyöppnad biograf i centrala Malmö.  Vi siktar på ett erbjuda en unik filmupplevelse med den bästa och nyaste tekniken på marknaden </p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, aliquam perspiciatis modi voluptatum facere nesciunt, aliquid aperiam at accusamus tempora, nulla vitae autem magni laborum illum expedita quibusdam itaque doloremque! Ex qui tempore nobis, perferendis, consequatur omnis quo optio eligendi aperiam, possimus ducimus voluptate quaerat modi deserunt quod. Similique itaque veniam consequatur fuga distinctio accusamus cumque error vel dolorum quibusdam.Porro quidem atque quisquam maxime, doloribus facere suscipit accusantium qui in natus eius eaque optio similique minus, iure adipisci. Blanditiis officia magni ut perferendis vero rem est quidem, dolore magnam?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae minus consequuntur, aliquid sapiente alias possimus numquam, cupiditate earum vitae iure labore debitis incidunt quam? Iure quae quasi ea aperiam inventore.</p>
+
+    <div className="bilder">
+      <img src='./images/OmOss/Biograf_inside.jpg'></img>
+      <img src='./images/OmOss/Biograf_outside.jpg'></img>
+    </div>
   </>
 }
