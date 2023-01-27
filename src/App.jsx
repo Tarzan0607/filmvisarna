ff32d3e32d423ac21695bb3d6789777de131861b
PGamingHD <pontus.2003@hotmail.com>
2023-01-27 17:17:17 +0100

    Latest details page with basic styling
    Added latest details updated page and added basic styling for page, also changed a couple of file names for easier navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useEffect } from 'react';
 import { useStates } from './utilities/states';
 import { urlify } from './utilities/urlify';
-import Footer from './components/main/Footer';
-import Navbar from './components/main/Navbar';
-import Home from './components/main/Home';
-import Error from './components/main/Error';
-import moveDetails from './components/movieDetails';
+import Footer from './components/pages/Footer';
+import Navbar from './components/pages/Navbar';
+import Home from './components/pages/Home';
+import Error from './components/pages/Error';
+import MovieDetails from './components/pages/MovieDetails';
 import {
   BrowserRouter,
   Routes,
   Route
 } from 'react-router-dom';
-import movieList from './components/movie/MovieList';
 
 export default function App() {
 
@@ -19,7 +18,7 @@ export default function App() {
     films: [],
     routes: [
       { path: '*', Component: Error },
-      { path: '/:moviePath', Component: moveDetails },
+      { path: '/movie/:MoviePath', Component: MovieDetails },
       { menuLabel: 'Start', path: '/', Component: Home }
     ]
   });
@@ -33,9 +32,9 @@ export default function App() {
       // convert it from json to a js data structure
       let dataFromJson = await fetchedData.json();
       // set a new value for the state variable people
-      s.films = dataFromJson;
-      for (let movie of s.films) {
-        movie.path = '/' + urlify(movie.title)
+      const films = dataFromJson;
+      for (let movie of films) {
+        movie.path = '/movie/' + urlify(movie.title)
       }
       s.films = films
       // oneliner:
