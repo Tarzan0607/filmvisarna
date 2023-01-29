a07305016d7bf3e17cb332be65af60e62e206482
Zoritta <113990865+Zoritta@users.noreply.github.com>
2023-01-29 21:25:26 +0100

    Update Spelschema.jsx

--- src/components/main/Spelschema.jsx
+++ src/components/main/Spelschema.jsx
@@ -1,13 +1,47 @@
-import { useState } from "react";
-import {
-  useEffect
-} from "react"
-import { useStates } from "../../utilities/states";
+import React from 'react';
 
+const Spelschema = () => {
+  return (
+    <div>
+      <h1>Spelschema</h1>
+      <div>
+        <h2>Stora Salong</h2>
+        <div>
+          <p>Movie: The Matrix</p>
+          <p>Poster: <img src="the_matrix_poster.jpg" alt="The Matrix poster" /></p>
+          <p>Datum: 2022-02-20</p>
+          <p>Tid: 14:00</p>
+        </div>
+        <div>
+          <p>Movie: Inception</p>
+          <p>Poster: <img src="inception_poster.jpg" alt="Inception poster" /></p>
+          <p>Datum: 2022-02-21</p>
+          <p>Tid: 16:00</p>
+        </div>
+      </div>
+      <div>
+        <h2>Lilla Salong</h2>
+        <div>
+          <p>Movie: The Dark Knight</p>
+          <p>Poster: <img src="the_dark_knight_poster.jpg" alt="The Dark Knight poster" /></p>
+          <p>Datum: 2022-02-20</p>
+          <p>Tid: 12:00</p>
+        </div>
+        <div>
+          <p>Movie: The Shawshank Redemption</p>
+          <p>Poster: <img src="shawshank_redemption_poster.jpg" alt="The Shawshank Redemption poster" /></p>
+          <p>Datum: 2022-02-21</p>
+          <p>Tid: 18:00</p>
+        </div>
+        <div>
+          <p>Movie: Pulp Fiction</p>
+          <p>Poster: <img src="pulp_fiction_poster.jpg" alt="Pulp Fiction poster" /></p>
+          <p>Datum: 2022-02-22</p>
+          <p>Tid: 20:00</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default function Spelschema() {
-  const s = useStates("main")
-
-  return <>hfskfhkf</>
-
-}
+export default Spelschema;
