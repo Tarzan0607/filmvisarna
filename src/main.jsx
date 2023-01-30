8fd04e44f5be70c5fe98c748501e0a65b304e8d8
DennisNevback <74974974+DennisNevback@users.noreply.github.com>
2023-01-30 13:43:05 +0100

    Footer updated
    Added css file for footer. Fixed styling and color on footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import './css/style.css';
 import './utilities/auto-key-lists';
 import './css/style-om-oss.css';
 import './css/style-butik.css';
+import './css/style-footer.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
