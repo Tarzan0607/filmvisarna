c22b3c102a6c2cbd23a1c2b803e60068ece5194b
Zoritta <113990865+Zoritta@users.noreply.github.com>
2023-01-30 13:25:00 +0100

    trial message

--- src/components/main/Spelschema.jsx
+++ src/components/main/Spelschema.jsx
@@ -8,13 +8,13 @@ const Spelschema = () => {
         <h2>Stora Salong</h2>
         <div>
           <p>Movie: Call me by your name</p>
-          <p>Poster: <img src="images\call-me-by-your-name.jpg" alt="Call me by your name poster" /></p>
+          <p>Poster: <img src="./images/call-me-by-your-name.jpg" alt="Call me by your name poster" /></p>
           <p>Datum: 2022-02-20</p>
           <p>Tid: 14:00</p>
         </div>
         <div>
           <p>Movie: Dune</p>
-          <p>Poster: <img src="images\dune.jpg" alt="Dune poster" /></p>
+          <p>Poster: <img src="./images/dune.jpg" alt="Dune poster" /></p>
           <p>Datum: 2022-02-21</p>
           <p>Tid: 16:00</p>
         </div>
@@ -23,19 +23,19 @@ const Spelschema = () => {
         <h2>Lilla Salong</h2>
         <div>
           <p>Movie: Spider-man: Into the Spider-verse</p>
-          <p>Poster: <img src=" images\into-the-spider---verse.jpg" alt="Spider-man: Into the Spider-verse" /></p>
+          <p>Poster: <img src=" ./images/into-the-spider---verse.jpg" alt="Spider-man: Into the Spider-verse" /></p>
           <p>Datum: 2022-02-20</p>
           <p>Tid: 12:00</p>
         </div>
         <div>
           <p>Movie: Sagan Om Konungens Återkomst</p>
-          <p>Poster: <img src=" images\the-return-of-the-king.jpg" alt="Sagan Om Konungens Återkomst" /></p>
+          <p>Poster: <img src=" ./images/the-return-of-the-king.jpg" alt="Sagan Om Konungens Återkomst" /></p>
           <p>Datum: 2022-02-21</p>
           <p>Tid: 18:00</p>
         </div>
         <div>
           <p>Movie: John Wick</p>
-          <p>Poster: <img src=" images\john-wick.jpg" alt="John Wick poster" /></p>
+          <p>Poster: <img src=" ./images/john-wick.jpg" alt="John Wick poster" /></p>
           <p>Datum: 2022-02-22</p>
           <p>Tid: 20:00</p>
         </div>
