afacc5c2030500b31f0caa0fc1d5f990bc13e078
DennisNevback <74974974+DennisNevback@users.noreply.github.com>
2023-01-31 17:14:28 +0100

    fixed image not loading
    fixed image not loading in movieDetails

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -8,9 +8,9 @@ export default function Home() {
       <p>Östra Fejkadress 28, 213 44 Malmö </p>
       <p>040 - 586 33 00 </p>
       <p>info@filmvisarna.se </p>
-      <img src='./images/Twitter_icon.png' />
-      <img src='./images/Facebook_icon.jpg' />
-      <img src='./images/Instagram_icon.png' />
+      <img src='/images/Twitter_icon.png' />
+      <img src='/images/Facebook_icon.jpg' />
+      <img src='/images/Instagram_icon.png' />
     </div>
   </>
 }
