2ea0b1c9217ee165e49482ba9850502f2c55ec9b
Zoritta <113990865+Zoritta@users.noreply.github.com>
2023-02-01 10:21:32 +0100

    import spelschema css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './utilities/auto-key-lists';
 import './css/style-om-oss.css';
 import './css/style-butik.css';
 import './css/style-footer.css';
+import './css/style-spelschema.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
