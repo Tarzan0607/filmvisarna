e80f4091a746a2a09396f5c574dff956a5064a9b
DennisNevback <74974974+DennisNevback@users.noreply.github.com>
2023-02-03 16:14:23 +0100

    Fixed p tags
    Fixed margins in <p> tags. reduced length in lorem ipsum

--- src/components/pages/Butik.jsx
+++ src/components/pages/Butik.jsx
@@ -3,12 +3,13 @@ export default function Butik() {
 
   return <>
     <h2 className="h2butik">Butiken</h2>
-    <p>I lobbyn finner du vår butik som erbjuder godis, läsk och lättare snacks från flera populära aktörer</p>
+    <div className="textbutik">
+      <p>I lobbyn finner du vår butik som erbjuder godis, läsk och lättare snacks från flera populära aktörer</p>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, aliquam perspiciatis modi voluptatum facere nesciunt, aliquid aperiam at accusamus tempora, nulla vitae autem magni laborum illum expedita quibusdam itaque doloremque! Ex qui tempore nobis, perferendis, consequatur omnis quo optio eligendi aperiam, possimus ducimus voluptate quaerat modi deserunt quod. Similique itaque veniam consequatur fuga distinctio accusamus cumque error vel dolorum quibusdam.Porro quidem atque quisquam maxime, doloribus facere suscipit accusantium qui in natus eius eaque optio similique minus, iure adipisci. Blanditiis officia magni ut perferendis vero rem est quidem, dolore magnam?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, aliquam perspiciatis modi voluptatum facere nesciunt, aliquid aperiam at accusamus tempora, nulla vitae autem magni laborum illum expedita quibusdam itaque doloremque! Ex qui tempore nobis, perferendis, consequatur omnis quo optio eligendi aperiam, possimus ducimus voluptate quaerat modi deserunt quod. Similique itaque veniam consequatur fuga distinctio accusamus cumque error vel dolorum quibusdam.</p>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae minus consequuntur, aliquid sapiente alias possimus numquam, cupiditate earum vitae iure labore debitis incidunt quam? Iure quae quasi ea aperiam inventore.</p>
-
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae minus consequuntur, aliquid sapiente alias possimus numquam, cupiditate earum vitae iure labore debitis incidunt quam? Iure quae quasi ea aperiam inventore.</p>
+    </div>
     <div className="bilder-butik">
       <img src='./images/Butik/Coca-Cola_iconsvg.png'></img>
       <img src='./images/Butik/Fanta_icons(2016).png'></img>
