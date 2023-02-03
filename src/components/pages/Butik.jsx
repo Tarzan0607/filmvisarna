c32b05d7939dcd4e4d4861e68a56c86d7749891a
DennisNevback <74974974+DennisNevback@users.noreply.github.com>
2023-02-03 16:42:04 +0100

    fixed p
    Fixed margin between p

--- src/components/pages/Butik.jsx
+++ src/components/pages/Butik.jsx
@@ -6,7 +6,7 @@ export default function Butik() {
     <div className="textbutik">
       <p>I lobbyn finner du vår butik som erbjuder godis, läsk och lättare snacks från flera populära aktörer</p>
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, aliquam perspiciatis modi voluptatum facere nesciunt, aliquid aperiam at accusamus tempora, nulla vitae autem magni laborum illum expedita quibusdam itaque doloremque! Ex qui tempore nobis, perferendis, consequatur omnis quo optio eligendi aperiam, possimus ducimus voluptate quaerat modi deserunt quod. Similique itaque veniam consequatur fuga distinctio accusamus cumque error vel dolorum quibusdam.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, aliquam perspiciatis modi voluptatum facere nesciunt, aliquid aperiam at accusamus tempora, nulla vitae autem magni laborum illum expedita quibusdam itaque doloremque! .</p>
 
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae minus consequuntur, aliquid sapiente alias possimus numquam, cupiditate earum vitae iure labore debitis incidunt quam? Iure quae quasi ea aperiam inventore.</p>
     </div>
