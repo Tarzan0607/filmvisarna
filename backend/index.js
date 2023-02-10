d6f8a6a3438199863cc2f6cd43bf6bae8c6419d5
PGamingHD <pontus.2003@hotmail.com>
2023-02-10 15:48:25 +0100

    Removed the anticrash.js, doesn't help us in any way!
    Removed the anticrash.js, doesn't help us in any way!

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ const express =  require('express');
 const mainRouter = require('./routes/router.js');
 const server = express();
 
-require('./utilities/anticrash.js');
-
 server.use('/api', mainRouter);
 
 server.listen(4000, () => console.log(`[BACKEND] <==> || Successfully started Backend Server! || <==> [BACKEND]`));
