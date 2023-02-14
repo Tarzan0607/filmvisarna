35eabf216ae63221fb8900d5803b2a5272485dcd
Swakshar Dutta <tarzan0607@gmail.com>
2023-02-14 22:10:00 +0100

    title remove

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,6 @@ import MovieList from '../movie/MovieList';
 
 export default function Home() {
     return<>
-        <h1 className='movieTitle'>All available Films</h1>
         <MovieList />
     </>
 }
